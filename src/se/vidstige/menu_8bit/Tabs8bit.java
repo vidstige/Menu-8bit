7681bc74dcd42dd5ee83488c8154a1557df8a66b
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-01-30 15:12:37 +0100

    Adding pressed state.

--- src/se/vidstige/menu_8bit/Tabs8bit.java
+++ src/se/vidstige/menu_8bit/Tabs8bit.java
@@ -6,7 +6,6 @@ import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
